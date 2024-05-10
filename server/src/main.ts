ae75550972ec67416e9575dafc05cdf2d4232661
hubol01 <kuloleg52@gmail.com>
2024-05-10 19:06:20 +0300

    add service, controller, entity for users

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,27 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3001);
+// }
+// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('LinguaQuest')
+    .setDescription('The LinguaQuest API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
---
+++ server/src/users/dto/create-user.dto.ts
@@ -0,0 +1,55 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  VKID?: string;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  TelegramID?: string;
+
+  @ApiProperty({ required: false, example: 'test@test.com' })
+  @IsString()
+  @IsOptional()
+  email?: string;
+
+  @ApiProperty({ example: 'test' })
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  username: string;
+
+  @ApiProperty({ required: false, example: 'user01' })
+  @IsString()
+  @IsOptional()
+  idUsername?: string;
+
+  @ApiProperty({ required: false, default: false })
+  @IsBoolean()
+  isAdmin: boolean;
+
+  @ApiProperty({ required: false, default: false })
+  isBan: boolean;
+
+  @ApiProperty({ required: false })
+  @IsNumber()
+  streak: number;
+
+  @ApiProperty({ required: false })
+  @IsNumber()
+  xp: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  date_created?: Date;
+}
