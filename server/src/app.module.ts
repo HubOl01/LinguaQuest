b5937b683823ade2e372d74bfe0feaa6915a732f
Sienbrit <kargadesh@gmail.com>
2024-05-13 15:51:47 +0300

    added languages

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { LanguagesModule } from './languages/languages.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule, PrismaModule, LanguagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/languages/dto/create-language.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateLanguageDto {
+  @ApiProperty({ required: false })
+  @IsOptional()
+  date_created?: Date; //Необязательны к вводу
+
+  @ApiProperty()
+  @IsString() 
+  @IsNotEmpty()           
+  language:string; //Обязательны к вводу
+
+  @ApiProperty()
+  @IsString()
+  flagUrl?:string;
+}
