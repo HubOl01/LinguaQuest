5db277f4bc7e4d12ce5a033593aa070a72ce3a6d
Oleg Kulichkov <kuloleg52@gmail.com>
2024-05-15 14:38:25 +0300

    add libs

--- mobile/ios/Runner/GeneratedPluginRegistrant.m
+++ mobile/ios/Runner/GeneratedPluginRegistrant.m
@@ -12,31 +12,41 @@
 @import flutter_keyboard_visibility;
 #endif
 
+<<<<<<< HEAD
 #if __has_include(<fluttertoast/FluttertoastPlugin.h>)
 #import <fluttertoast/FluttertoastPlugin.h>
 #else
 @import fluttertoast;
 #endif
 
+=======
+>>>>>>> 564eb98 (add libs)
 #if __has_include(<path_provider_foundation/PathProviderPlugin.h>)
 #import <path_provider_foundation/PathProviderPlugin.h>
 #else
 @import path_provider_foundation;
 #endif
 
+<<<<<<< HEAD
 #if __has_include(<sqflite/SqflitePlugin.h>)
 #import <sqflite/SqflitePlugin.h>
 #else
 @import sqflite;
 #endif
 
+=======
+>>>>>>> 564eb98 (add libs)
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FlutterKeyboardVisibilityPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterKeyboardVisibilityPlugin"]];
+<<<<<<< HEAD
   [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
   [PathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"PathProviderPlugin"]];
   [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+=======
+  [PathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"PathProviderPlugin"]];
+>>>>>>> 564eb98 (add libs)
 }
 
 @end
