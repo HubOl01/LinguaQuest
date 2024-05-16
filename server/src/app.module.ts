2c5d08320eed735aa658a3e3ff5a5333d2bc7d33
Sienbrit <kargadesh@gmail.com>
2024-05-16 21:44:35 +0300

    Whoops

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -14,22 +14,20 @@ import { LanguagesModule } from './languages/languages.module';
 import { ModulesModule } from './modules/modules.module';
 
 import { LessonsModule } from './lessons/lessons.module';
-import { QuizTypesModule } from './quiz-types/quiz-types.module';
-import { QuizTestsModule } from './quiz-tests/quiz-tests.module';
-import { UserAnswersModule } from './user-answers/user-answers.module';
-import { StudyHistorysModule } from './study-historys/study-historys.module';
-
-import { RulesModule } from './rules/rules.module';
-import { DictionariesModule } from './dictionaries/dictionaries.module';
-import { CardwordsModule } from './cardwords/cardwords.module';
-import { GlobaldictionariesModule } from './globaldictionaries/globaldictionaries.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
-
+import { UserAnswersModule } from './user-answers/user-answers.module';
+import { GlobaldictionariesModule } from './globaldictionaries/globaldictionaries.module';
 import { AchievementsModule } from './achievements/achievements.module';
 import { AvatarModule } from './avatar/avatar.module';
-import { WeeklyRecordsModule } from './weekly-records/weekly-records.module';
+import { CardwordsModule } from './cardwords/cardwords.module';
+import { DictionariesModule } from './dictionaries/dictionaries.module';
 import { LeaguesModule } from './leagues/leagues.module';
+import { QuizTestsModule } from './quiz-tests/quiz-tests.module';
+import { QuizTypesModule } from './quiz-types/quiz-types.module';
 import { QuizsModule } from './quizs/quizs.module';
+import { RulesModule } from './rules/rules.module';
+import { StudyHistorysModule } from './study-historys/study-historys.module';
+import { WeeklyRecordsModule } from './weekly-records/weekly-records.module';
 
 
 @Module({
