6bd924e1a7d34146e60e2de717dbc554037c3ead
hubol01 <kuloleg52@gmail.com>
2024-05-16 21:57:53 +0300

    add pages

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'LinguaQuest',
       theme: ThemeData(
        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
