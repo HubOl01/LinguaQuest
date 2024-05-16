f60bb78849ae805cd551fe36d517d5e114263fb6
Sienbrit <kargadesh@gmail.com>
2024-05-16 17:58:29 +0300

    finished lessons

--- server/src/lessons/lessons.module.ts
+++ server/src/lessons/lessons.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LessonsService } from './lessons.service';
 import { LessonsController } from './lessons.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [LessonsController],
   providers: [LessonsService],
+  imports: [PrismaModule]
 })
 export class LessonsModule {}
