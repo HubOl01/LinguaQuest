2574d1101aa5ac759bab8702171a53df833b96fb
Oleg Kulichkov <kuloleg52@gmail.com>
2024-05-16 22:07:30 +0300

    Revert "Added lessons and quizzes (latter works poorly)"

--- server/src/lessons/lessons.module.ts
+++ server/src/lessons/lessons.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LessonsService } from './lessons.service';
 import { LessonsController } from './lessons.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [LessonsController],
   providers: [LessonsService],
-  imports: [PrismaModule]
 })
 export class LessonsModule {}
