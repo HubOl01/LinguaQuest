d5dc4c752b3328197d003a3c9e619393cf248e71
Oleg Kulichkov <kuloleg52@gmail.com>
2024-05-17 00:32:52 +0300

    add

--- mobile/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ mobile/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/foward01/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/foward01/MyProjects/LinguaQuest/mobile"
+export "FLUTTER_ROOT=C:\dev\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\LinguaQuest\mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
