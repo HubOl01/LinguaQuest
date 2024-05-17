ef9abe9c0f8241c6954e8859af26aa753403df48
Oleg Kulichkov <kuloleg52@gmail.com>
2024-05-17 13:48:10 +0300

    add Profile

---
+++ mobile/lib/pages/posts/postsPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostPage extends StatefulWidget {
+  const PostPage({super.key});
+
+  @override
+  State<PostPage> createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
