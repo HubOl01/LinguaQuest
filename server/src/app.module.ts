5af4e4a21a27339b1b34bdd8f1c3498862232772
Шестернев Даниил <danil.krapiva103@mail.ru>
2024-05-18 16:24:15 +0300

    modules done

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -11,23 +11,25 @@ import { LikesModule } from './likes/likes.module';
 import { ViewsModule } from './views/views.module';
 import { CommentsModule } from './comments/comments.module';
 import { LanguagesModule } from './languages/languages.module';
-import { ModulesModule } from './modules/modules.module';
 
 import { LessonsModule } from './lessons/lessons.module';
-import { BookmarksModule } from './bookmarks/bookmarks.module';
+
 import { UserAnswersModule } from './user-answers/user-answers.module';
-import { GlobaldictionariesModule } from './globaldictionaries/globaldictionaries.module';
 import { AchievementsModule } from './achievements/achievements.module';
 import { AvatarModule } from './avatar/avatar.module';
-import { CardwordsModule } from './cardwords/cardwords.module';
-import { DictionariesModule } from './dictionaries/dictionaries.module';
+
 import { LeaguesModule } from './leagues/leagues.module';
 import { QuizTestsModule } from './quiz-tests/quiz-tests.module';
 import { QuizTypesModule } from './quiz-types/quiz-types.module';
-import { RulesModule } from './rules/rules.module';
 import { StudyHistorysModule } from './study-historys/study-historys.module';
 import { WeeklyRecordsModule } from './weekly-records/weekly-records.module';
 
+import { RulesModule } from './rules/rules.module';
+import { DictionariesModule } from './dictionaries/dictionaries.module';
+import { CardwordsModule } from './cardwords/cardwords.module';
+import { GlobaldictionariesModule } from './globaldictionaries/globaldictionaries.module';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
   imports: [
@@ -56,7 +58,12 @@ import { WeeklyRecordsModule } from './weekly-records/weekly-records.module';
     StudyHistorysModule,
     LanguagesModule, 
     ModulesModule, 
-    LessonsModule, QuizsModule, AchievementsModule, AvatarModule, WeeklyRecordsModule, LeaguesModule
+    LessonsModule,
+    RulesModule,
+    DictionariesModule,
+    CardwordsModule,
+    GlobaldictionariesModule,
+    BookmarksModule
   ],
   controllers: [AppController],
   providers: [AppService],
